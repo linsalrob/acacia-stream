bc51333532131c36899a246d3fd03643360b63b4
linsalrob <raedwards@gmail.com>
2023-09-18 20:52:25 +0800

    Search against an S3 human

---
+++ examples/human_mappy.py
@@ -0,0 +1,70 @@
+"""
+Map a fastx file against the human genome that is on acia
+
+This is based on https://github.com/lh3/minimap2/blob/master/python/minimap2.py  (among other things!)
+"""
+
+import os
+import sys
+import argparse
+
+import boto3
+import mappy as mp
+
+__author__ = 'Rob Edwards'
+
+
+def get_human_genome(location='databases/human/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz'):
+    """
+    Get the human genome
+    """
+
+    bucket_name, wanted = location.split('/', 1)
+
+    session = boto3.session.Session()
+    s3_client = session.client(
+        service_name='s3',
+        endpoint_url='https://projects.pawsey.org.au',
+    )
+
+    # we could go directly, but this runs some sanity checks on our request
+    buckets = set()
+    for bucket in s3_client.list_buckets()['Buckets']:
+        buckets.add(bucket['Name'])
+
+    if bucket_name not in buckets:
+        print(f"Sorry, {bucket_name} not found in your available buckets. Your choices are:", file=sys.stderr)
+        print("\n".join(buckets), file=sys.stderr)
+        sys.exit(2)
+
+    for obj in s3_client.list_objects(Bucket=bucket_name)['Contents']:
+        if obj['Key'] == wanted:
+            return s3_client.get_object(Bucket=bucket_name, Key=wanted)['Body'].read()
+
+
+def read_align(reads, preset, min_cnt = None, min_sc = None, k = None, w = None, bw = None, out_cs = False, verbose=False):
+
+
+    a = mp.Aligner(get_human_genome(), preset=preset, min_cnt=min_cnt, min_chain_score=min_sc, k=k, w=w, bw=bw)
+    if not a:
+        raise Exception("ERROR: failed to load/build index file for the human genome")
+    for name, seq, qual in mp.fastx_read(reads): # read one sequence
+        for h in a.map(seq, cs=out_cs): # traverse hits
+            print('{}\t{}\t{}'.format(name, len(seq), h))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Usage: human_mappy.py [options] <ref.fa>|<ref.mmi> <query.fq>')
+    parser.add_argument('-f', help='input file', required=True)
+    parser.add_argument('-x', help='preset: sr, map-pb, map-ont, asm5, asm10 or splice', default='sr')
+    parser.add_argument('-n', help='mininum number of minimizers', type=int)
+    parser.add_argument('-m', help='mininum chaining score', type=int)
+    parser.add_argument('-k', help='k-mer length', type=int)
+    parser.add_argument('-w', help='minimizer window length', type=int)
+    parser.add_argument('-r', help='band width', type=int)
+    parser.add_argument('-c', help='output the cs tag', action='store_true')
+    parser.add_argument('-v', help='verbose output', action='store_true')
+    args = parser.parse_args()
+
+    read_align(reads=args.f, preset=args.x, min_cnt=args.n, min_sc=args.m, k=args.k, w=args.w, bw=args.r, out_cs=args.c, verbose=args.v)
+
