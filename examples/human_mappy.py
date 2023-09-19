5d3e2834f80561e837f52cfa0b55b2c7253c7cd8
linsalrob <raedwards@gmail.com>
2023-09-19 14:58:29 +0800

    Multiprocessing!

--- examples/human_mappy.py
+++ examples/human_mappy.py
@@ -11,12 +11,13 @@ import argparse
 import zlib
 import boto3
 import mappy as mp
+from multiprocessing import Process
 
 __author__ = 'Rob Edwards'
 
 
 # def get_human_genome(location='databases/human/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.gz', verbose=False):
-def get_human_genome(location='databases/human/chr1.fna.gz', verbose=False):
+def get_human_genome(location, verbose=False):
     """
     Get the human genome
     """
@@ -52,10 +53,36 @@ def get_human_genome(location='databases/human/chr1.fna.gz', verbose=False):
                 return s3_client.get_object(Bucket=bucket_name, Key=wanted)['Body'].read()
             """
 
+
+def write_the_genome(human_genome, fifo, verbose=False):
+    """
+    A function to write the genome
+    """
+    stream = get_human_genome(human_genome, verbose=verbose)
+    fd = os.open(fifo, os.O_RDWR | os.O_NONBLOCK)
+    print(f"FD is {fd} from {os.getpid()} a child of {os.getppid()}", file=sys.stderr)
+    with io.FileIO(fd, 'wb') as f:
+        print("IO file opened", file=sys.stderr)
+        f.write(stream.read())
+
+
+def read_genome(fifo, reads, preset, min_cnt = None, min_sc = None, k = None, w = None, bw = None, out_cs = False, verbose=False):
+    """
+    Read the genome from the fifo and return the alignment object
+    """
+    if verbose:
+        print("Connecting reader", file=sys.stderr)
+    a = mp.Aligner(fifo, preset=preset, min_cnt=min_cnt, min_chain_score=min_sc, k=k, w=w, bw=bw)
+    print(f"We got {a}", file=sys.stderr)
+    if not a:
+        raise Exception("ERROR: failed to load/build index file for the human genome")
+    for name, seq, qual in mp.fastx_read(reads):  # read one sequence
+        print(f"Read {name}")
+        for h in a.map(seq, cs=out_cs):  # traverse hits
+            print('{}\t{}\t{}'.format(name, len(seq), h))
+
+
 def read_align(reads, preset, min_cnt = None, min_sc = None, k = None, w = None, bw = None, out_cs = False, verbose=False):
-
-
-    stream = get_human_genome(verbose=verbose)
 
     # here we create a fifo object that we can pass to the mp.Aligner
     FIFO_PATH = f'/home/edwa0468/scratch/human/tmp.{os.getpid()}.fna.gz'
@@ -63,30 +90,22 @@ def read_align(reads, preset, min_cnt = None, min_sc = None, k = None, w = None,
         print(f"ERROR: {FIFO_PATH} exists. Not overwriting", file=sys.stderr)
         sys.exit(2)
     os.mkfifo(FIFO_PATH)
-    fd = os.open(FIFO_PATH, os.O_RDWR | os.O_NONBLOCK)
-    print(f"FD is {fd}", file=sys.stderr)
-    with io.FileIO(fd, 'wb') as f:
-        print("Opened", file=sys.stderr)
-        f.write(stream.read())
-
     print(f"Our FIFO is at {FIFO_PATH}", file=sys.stderr)
+    writeprocess = Process(target=write_the_genome, args=(FIFO_PATH,))
+    writeprocess.start()
 
     if verbose:
         print("Opening the aligner", file=sys.stderr)
 
-    a = mp.Aligner(FIFO_PATH, preset=preset, min_cnt=min_cnt, min_chain_score=min_sc, k=k, w=w, bw=bw)
-    print(f"We got {a}", file=sys.stderr)
-    if not a:
-        raise Exception("ERROR: failed to load/build index file for the human genome")
-    for name, seq, qual in mp.fastx_read(reads): # read one sequence
-        print(f"Read {name}")
-        for h in a.map(seq, cs=out_cs): # traverse hits
-            print('{}\t{}\t{}'.format(name, len(seq), h))
+    readprocess = Process(target=read_genome(FIFO_PATH, preset, min_cnt, min_sc, k, w, bw, out_cs))
+    readprocess.join()
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Usage: human_mappy.py [options] <ref.fa>|<ref.mmi> <query.fq>')
     parser.add_argument('-f', help='input file', required=True)
+    parser.add_argument('-g', help='human genome', default='databases/human/chr1.fna.gz')
     parser.add_argument('-x', help='preset: sr, map-pb, map-ont, asm5, asm10 or splice', default='sr')
     parser.add_argument('-n', help='mininum number of minimizers', type=int)
     parser.add_argument('-m', help='mininum chaining score', type=int)
